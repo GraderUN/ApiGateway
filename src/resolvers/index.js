5beff593b3537f24078de0af957d3e85663e4d29
jdacostabe <jdacostabe@unal.edu.co>
2020-10-03 16:41:28 -0500

    Operaciones Básicas completas

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,5 @@
-const userResolver = require('./user_resolver');
+const coursesResolver = require('./courses_resolver');
 const lodash = require('lodash');
-const resolvers = lodash.merge(userResolver);
+const resolvers = lodash.merge(coursesResolver);
 
 module.exports = resolvers;
--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,8 +1,8 @@
 //Se llama al typedef (esquema) de cada submodulo
-const userTypeDefs = require('./user_type_defs');
+const courseTypeDefs = require('./course_type_defs');
 
 //Se unen
-const schemasArrays = [userTypeDefs];
+const schemasArrays = [courseTypeDefs];
 
 //Se exportan
-module.exports = schemasArrays; 
+module.exports = schemasArrays;
