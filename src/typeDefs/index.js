89d360bf80b4eb3f22087f564779aae6f2855499
jdacostabe <jdacostabe@unal.edu.co>
2020-10-06 10:52:52 -0500

    Correccion

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,6 +1,5 @@
 //Se llama al typedef (esquema) de cada submodulo
 const courseTypeDefs = require('./course_type_defs');
-const userTypeDefs = require('./user_type_defs');
 const notasTypeDefs = require('./notas_type_defs');
 const classroomTypeDefs = require('./classroom_type_defs');
 const generalTypeDefs = require('./mutationsYQuerys');
