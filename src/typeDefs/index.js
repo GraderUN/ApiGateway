10535f0e0bdd75346c59fa64ea09cd8a599858ce
samoralespu <samoralespu@unal.edu.co>
2020-10-05 22:08:40 -0500

    somethign added

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,8 +1,8 @@
 //Se llama al typedef (esquema) de cada submodulo
 const userTypeDefs = require('./user_type_defs');
-const notasTypeDEFS = require('./notas_type_defs');
+const notasTypeDefs = require('./notas_type_defs');
 //Se unen
-const schemasArrays = [userTypeDefs];
+const schemasArrays = [userTypeDefs, notasTypeDefs];
 
 //Se exportan
 module.exports = schemasArrays; 
