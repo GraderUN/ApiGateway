a8dd24161472eb7f595dc82acb92b3be55088dba
sanhernandezmon <sanhernandezmon@unal.edu.co>
2020-10-06 22:47:29 -0500

    ready to deploy :rocket:

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ApiGateway
 In this repo is being developed the business logic from the app
+
+```
+docker build apigateway .
+docker run -p 5000:5000 -e AUTH_URL = 'duque' -e  COURSES_URL = 'julian' -e CLASSROOM_URL = 'yo' -e NOTAS_URL = 'otro yo' -e SUBJECT_URL = 'camilo' -e NOTAS_NOTAS_URL 
+
+
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 module.exports = {
-    authUrl:      process.env.AUTH_URL       || 'ec2-3-237-0-162.compute-1.amazonaws.com',
-    authPort:     process.env.AUTH_AUTH_PORT || '8000',
-    coursesUrl:   process.env.COURSES_URL    || 'ec2-3-229-142-134.compute-1.amazonaws.com',
-    coursesPort:  process.env.AUTH_PORT      || '80',
-    classroomUrl: process.env.COURSES_URL    || 'ec2-18-207-254-125.compute-1.amazonaws.com',
-    classromPort: process.env.AUTH_PORT      || '8080',
-    notasUrl:     process.env.NOTAS_URL      || 'ec2-34-239-166-87.compute-1.amazonaws.com',
-    notasPort:    process.env.NOTAS_PORT     || '3000',
-    subjectUrl:   process.env.SUBJECT_URL    || 'localhost',
-    subjectPort:  process.env.SUBJECT_PORT   || '4000',
+    authUrl:      process.env.AUTH_URL          || 'ec2-3-237-0-162.compute-1.amazonaws.com',
+    authPort:     process.env.AUTH_PORT         || '8000',
+    coursesUrl:   process.env.COURSES_URL       || 'ec2-3-229-142-134.compute-1.amazonaws.com',
+    coursesPort:  process.env.COURSES_PORT      || '80',
+    classroomUrl: process.env.CLASSROOM_URL     || 'ec2-18-207-254-125.compute-1.amazonaws.com',
+    classromPort: process.env.CLASSROOM_PORT    || '8080',
+    notasUrl:     process.env.NOTAS_URL         || 'ec2-34-239-166-87.compute-1.amazonaws.com',
+    notasPort:    process.env.NOTAS_PORT        || '3000',
+    subjectUrl:   process.env.SUBJECT_URL       || 'localhost',
+    subjectPort:  process.env.SUBJECT_PORT      || '4000',
+    //AGREGAR AQUI NOTAS_NOTAS_URL Y PUERTO
 }
