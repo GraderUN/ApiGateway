37ffc62dbd47f17718a5f985ec4cadf62eb9f083
jcvar <36166613+jcvar@users.noreply.github.com>
2020-10-06 22:04:35 -0500

    Fix duplicate line

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,7 +2,6 @@ const coursesResolver = require('./courses_resolver');
 const notasResolver = require('./notas_resolver');
 const classroomResolver = require('./classroom_resolver')
 const AuthResolver = require('./auth_resolver')
-const classroomResolver = require('./classroom_resolver');
 const subjectResolver = require('./subject_resolver');
 const lodash = require('lodash');
 
