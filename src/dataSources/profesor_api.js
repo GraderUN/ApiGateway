2f2d13ea0da525898c89f9b90115cd9a1fd1343f
Francisco <franciscojavier.9a@gmail.com>
2020-10-20 15:32:36 -0500

    ports and urls only from env variables

--- src/dataSources/profesor_api.js
+++ src/dataSources/profesor_api.js
@@ -4,30 +4,43 @@ const serverConfig = require('../server');
 function ProfessorsMapping(data) {
   var newData = [];
   for (i = 0; i < data.length; i++)
-    newData[i] = { id: data[i].id_profesor, nombre: data[i].nombre, apellido: data[i].apellido, edad: data[i].edad,
-      telefono: data[i].telefono, email: data[i].email };
+    newData[i] = {
+      id: data[i].id_profesor,
+      nombre: data[i].nombre,
+      apellido: data[i].apellido,
+      edad: data[i].edad,
+      telefono: data[i].telefono,
+      email: data[i].email
+  };
   return newData;
 }
 
 function ProfessorMapping(data) {
-  return { id: data.id_profesor, nombre: data.nombre, apellido: data.apellido, edad: data.edad, telefono: data.telefono,
-    email: data.email };
+  return {
+    id: data.id_profesor,
+    nombre: data.nombre,
+    apellido: data.apellido,
+    edad: data.edad,
+    telefono: data.telefono,
+    email: data.email
+  };
 }
 
 function CreateProfessorMapping(data) {
-  return { nombre: data.nombre, apellido: data.apellido, edad: data.edad, telefono: data.telefono, email: data.email };
-}
-
-function ProfessorCreatedMapping(data) {
-  return { id: data.id_profesor, nombre: data.nombre, apellido: data.apellido, edad: data.edad, telefono: data.telefono,
-    email: data.email };
+  return {
+    nombre: data.nombre,
+    apellido: data.apellido,
+    edad: data.edad,
+    telefono: data.telefono,
+    email: data.email
+  };
 }
 
 class ProfesorAPI extends RESTDataSource {
 
   constructor() {
     super();
-    this.baseURL = `http://${serverConfig.profesorUrl}:${serverConfig.profesorPort}`;
+    this.baseURL = `http://${serverConfig.administrativoUrl}:${serverConfig.administrativoPort}`;
   }
 
   async getProfesorById(id) {
