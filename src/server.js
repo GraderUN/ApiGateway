6b677693033bc8f3976bbc31aa35cd4f21e0db18
sanhernandezmon <Shm1qazz>
2020-10-21 13:52:00 -0500

    hernandez query added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 module.exports = {
-    gestionNotasUrl: process.env.NOTAS_API_URL,
-    gestionNotasPort: process.env.NOTAS_API_PORT,
+    gestionNotasUrl: process.env.NOTAS_API_URL || "ec2-100-26-180-84.compute-1.amazonaws.com",
+    gestionNotasPort: process.env.NOTAS_API_PORT || "4000",
     authUrl: process.env.AUTH_URL,
     authPort: process.env.AUTH_PORT,
     coursesUrl: process.env.COURSES_URL || "ec2-3-236-208-131.compute-1.amazonaws.com",
-    coursesPort: process.env.COURSES_PORT || "8000",
+    coursesPort: process.env.COURSES_PORT || "80",
     classroomUrl: process.env.CLASSROOM_URL || "ec2-3-238-118-201.compute-1.amazonaws.com",
     classromPort: process.env.CLASSROOM_PORT || "8080",
     notasUrl: process.env.NOTAS_URL || "ec2-34-239-178-179.compute-1.amazonaws.com",
