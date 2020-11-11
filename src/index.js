05b8d45a4e78e1437e465ba576c549acb77d8650
jdacostabe <jdacostabe@unal.edu.co>
2020-11-10 20:01:16 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -44,8 +44,16 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
+app.get('/', function(req, res, next) {
+    response.send("");
+});
   
+app.post('/', function(req, res, next) {
+    response.send("");
+});
+
 apollo.applyMiddleware({ app })
 
 
